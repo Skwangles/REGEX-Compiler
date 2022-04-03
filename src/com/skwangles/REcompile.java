98b1dd4bde9433474e1e9dfae4cd8fd0522b86ec
Skwangles <tree_house@hotmail.co.nz>
2022-04-03 20:20:38 +1200

    Inital Commit

---
+++ src/com/skwangles/REcompile.java
@@ -0,0 +1,7 @@
+package com.skwangles;
+
+public class REcompile {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ src/com/skwangles/REsearch.java
@@ -0,0 +1,7 @@
+package com.skwangles;
+
+public class REsearch {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
